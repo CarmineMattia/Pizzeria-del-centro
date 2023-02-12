9cc6ca0ad10633116befe41e6eafdefc5b32280d
[carmine mattia] config --global user.email [98carmine3@gmail.com] <98carmine3@gmail.com>
2023-02-12 17:29:32 +0100

    3 commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+let prevScrollPos = window.pageYOffset;
+console.log(prevScrollPos);
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("nav").style.top = "0";
+    console.log("Prev: " + prevScrollPos);
+    console.log("Curr: " + currentScrollPos);
+  } else {
+    document.getElementById("nav").style.top = "-500px";
+    console.log("Prev: " + prevScrollPos);
+    console.log("Curr: " + currentScrollPos);
+  }
+  prevScrollPos = currentScrollPos;
+};
